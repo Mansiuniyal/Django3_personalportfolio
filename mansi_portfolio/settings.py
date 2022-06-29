64c8b350ba5be619b03a0fa48462b5463ff7a901
Mansi <mansiuniyal2703@gmail.com>
2022-06-29 12:27:49 +0000

    Remove old stuff

--- mansi_portfolio/settings.py
+++ mansi_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g(nxv-$(7kl7m@bop+9c^!z8@xwc%20lmak85j!=()mhe2h54e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['MansiUniyal.pythonanywhere.com']
 
 
 # Application definition
@@ -119,7 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT=BASE_DIR / 'static'
+STATIC_ROOT=BASE_DIR / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
